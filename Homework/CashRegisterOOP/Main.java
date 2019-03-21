6b623fb8c2e19b1b92912868951506a2b88b3c02
wangjason2020 <42677901+wangjason2020@users.noreply.github.com>
2019-03-20 22:55:01 -0700

    Create Main.java

---
+++ Homework/CashRegisterOOP/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Product apples = new Product("Apples",4131,false,.49,75,"Produce");
+        Product apricots = new Product("Apricots",4218,false,.39,75,"Produce");
+        Product bananas = new Product("Bananas", 4398, true,.59,75,"Produce");
+    }
+}
