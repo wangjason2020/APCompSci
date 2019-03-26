bd41945672d49ea7d0d98e43ad7de1638e6dec14
wangjason2020 <42677901+wangjason2020@users.noreply.github.com>
2019-03-26 08:23:07 -0700

    Update Main.java

--- Homework/Bank/Main.java
+++ Homework/Bank/Main.java
@@ -1,8 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static int balance=500;
+
     public static void main(String[] args) {
-    
-    //checking account, savings account, balance, customer, teller, atm
-    
+
+        viewBalance();
+        atm();
+        deposit();
+
+    }
+
+    public static void viewBalance() {
+        System.out.println("Balance: $"+balance);
+    }
+
+    public static void atm() {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("How much would you like to withdraw? ");
+        int withdrawal=sc.nextInt();
+        if (balance>=withdrawal) {
+            balance-=withdrawal;
+            System.out.println("Balance Remaining: $"+balance);
+        }
+        else {
+            System.out.print("You do not have enough money to do this.");
+        }
+    }
+
+    public static void deposit() {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("How much would you like to deposit? ");
+        int deposit=sc.nextInt();
+        balance+=deposit;
+        System.out.println("Balance Remaining: $"+balance);
     }
 }
