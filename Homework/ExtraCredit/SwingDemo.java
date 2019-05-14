51db9df352bef3060fb3d411b97ac4b57f996372
wangjason2020 <42677901+wangjason2020@users.noreply.github.com>
2019-05-14 10:12:09 -0700

    Create SwingDemo.java

---
+++ Homework/ExtraCredit/SwingDemo.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class SwingDemo {
+    JFrame frame=new JFrame("A Simple Swing Application");
+    public void setSize(int x, int y) {
+        frame.setSize(275,100);
+    }
+}
